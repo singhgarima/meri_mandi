a9ce12d04be900fc4a5b312412b287ae4b952cec
Garima Singh <garimas@ingarimas.corporate.thoughtworks.com>
2013-08-26 16:01:56 +0530

    Adding new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeriMandi::Application.config.secret_key_base = '32190f85fb5a41932f02a63e65b82c959b5f4df532a578deaa38faa41a0cca542065d5a94ce3361cd3693c5b01782ebc4906b6aed787d7f417d30791622f1458'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MeriMandi::Application.config.session_store :cookie_store, key: '_meri_mandi_session'
