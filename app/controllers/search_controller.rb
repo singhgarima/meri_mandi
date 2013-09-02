655b6194db0aa4974d867d3d178fd2d3ddf74996
Garima Singh <igarimasingh@gmail.com>
2013-09-02 20:47:47 +0530

    Adding Advanced Search criteria UI

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,15 @@ class SearchController < ApplicationController
 	end
 
 	def fetch
+    flash[:notice] = 'No search criteria mentioned' and render :index and 
+      return if search_criteria.all? {|s| params[s].blank? }
 		@results = {}
 		@results = CommodityPrice.search(params['query'], Date.today)
 		render :index
 	end
+
+  private
+  def search_criteria
+    ['query', 'date', 'location']
+  end
 end
