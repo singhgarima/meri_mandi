7a09a51165526535998f7566a161706caa35fbe9
Garima Singh <igarimasingh@gmail.com>
2013-09-13 16:22:10 +0530

    Spell Mistake

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/ec2-user/apps/meri_mani/current
+APP_ROOT=/home/ec2-user/apps/meri_mandi/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=ec2-user
